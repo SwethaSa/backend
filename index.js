8d02ed58dfbeed178fcc674d4816c3c7ab35eb3a
Raveena2030 <123936776+Raveena2030@users.noreply.github.com>
2024-02-08 21:29:32 +0530

    commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import cors from "cors";
 const app = express();
 dotenv.config();
 app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT;
 const MONGO_URL = process.env.MONGO_URL;
